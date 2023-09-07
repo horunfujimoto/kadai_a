a5135551e873960e9ebaed15bf25add5c0d561fa
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-09-07 04:08:30 +0000

    [Fix]応用課題9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book_detail = Book.find(params[:id])
+    @book_detail = Book.find(params[:id]) #閲覧数カウント
     unless ReadCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
       current_user.read_counts.create(book_id: @book_detail.id)
     end
